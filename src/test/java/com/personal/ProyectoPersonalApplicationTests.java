1963567f80ef82de15c0ad2e96cc6bb6bb48889a
FrHo24 <fhdezof24@gmail.com>
2022-10-03 14:50:21 -0600

    Prueba

---
+++ src/test/java/com/personal/ProyectoPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
